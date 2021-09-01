7767cf18d6d6f6216c9a4b231138277f42468fa4
ilazosol <ilazosol@everis.com>
2021-09-01 18:15:13 -0500

    cambio

--- src/main/java/com/everis/springboot/eureka/SpringbootEurekaServerApplication.java
+++ src/main/java/com/everis/springboot/eureka/SpringbootEurekaServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class SpringbootEurekaServerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootEurekaServerApplication.class, args);
 	}
